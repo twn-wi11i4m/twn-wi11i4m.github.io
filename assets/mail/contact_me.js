3233d1aab1764f1c58a6404174bdd1dfa9ee842c
Wi11iam <williamntw2@gmail.com>
2020-08-10 03:35:15 +0800

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -39,7 +39,7 @@ $(function () {
                         )
                         .append("</button>");
                     $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
+                        "<strong>Your message has been sent. I will contact you later.</strong>"
                     );
                     $("#success > .alert-success").append("</div>");
                     //clear all fields
