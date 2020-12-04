00a10b1e96a414e4bcee4d100f75b8d2bbd6f19f
Wi11iam <williamntw2@gmail.com>
2020-12-04 18:45:33 +0800

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -29,7 +29,7 @@ $(function () {
                     email: email,
                     message: message,
                 },
-                cache: false,
+                cache: true,
                 success: function () {
                     // Success message
                     $("#success").html("<div class='alert alert-success'>");
